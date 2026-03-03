ef3f4a0e6440b4c37bed88d468ecca33aab00531
HieuCuteDangYeu <nguyendinhhieu050505@gmail.com>
2026-03-03 15:17:39 +0700

    Update cookie path in logout response to root

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -196,7 +196,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      path: '/auth',
+      path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
